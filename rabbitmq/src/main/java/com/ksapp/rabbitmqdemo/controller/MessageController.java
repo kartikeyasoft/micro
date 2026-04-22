40beadfd9766a5a0550f02b17baf3f6e60436293
Your Name <you@example.com>
2026-04-22 17:09:58 +0530

    ..

---
+++ rabbitmq/src/main/java/com/ksapp/rabbitmqdemo/controller/MessageController.java
@@ -0,0 +1,41 @@
+package com.ksapp.rabbitmqdemo.controller;
+
+import com.ksapp.rabbitmqdemo.service.MessageProducer;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+@CrossOrigin("*")
+public class MessageController {
+
+    @Value("${redis.service.url:http://localhost:1222/redis}")
+    private String redisServiceUrl;
+
+    @Autowired
+    private MessageProducer producer;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private RestTemplate template;
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
+        rabbitTemplate.convertAndSend("my-exchange", "my-routing-key", message);
+        return ResponseEntity.ok("Message sent to RabbitMQ");
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<?> getNotificationHistory() {
+        List<Object> history = template.getForObject(redisServiceUrl + "/history", List.class);
+        return ResponseEntity.ok(history);
+    }
+}
