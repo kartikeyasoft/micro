be1f82c5808a18cea2bdd5299bcd2a451230c043
kartikeya <64151789+kartikeyasoft@users.noreply.github.com>
2026-04-22 15:36:01 +0530

    Update MessageController.java

--- rabbitmq/src/main/java/com/example/rabbitmqdemo/controller/MessageController.java
+++ rabbitmq/src/main/java/com/example/rabbitmqdemo/controller/MessageController.java
@@ -3,10 +3,8 @@ package com.example.rabbitmqdemo.controller;
 import com.example.rabbitmqdemo.service.MessageProducer;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,56 +15,27 @@ import java.util.List;
 @CrossOrigin("*")
 public class MessageController {
 
+    @Value("${redis.service.url:http://localhost:1222/redis}")
+    private String redisServiceUrl;
+
     @Autowired
     private MessageProducer producer;
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-//    @Autowired
-//    private RedisTemplate<String, Object> redisTemplate;
-@Autowired
-private RabbitTemplate rabbitTemplate;
-
 
     @Autowired
-    private RestTemplate  template;
+    private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private RestTemplate template;
 
-//    @PostMapping("/send")
-//    public ResponseEntity<String> sendMessage(@RequestBody String message) {
-//        producer.send(message);
-//        messagingTemplate.convertAndSend("/topic/messages", message);
-//        return ResponseEntity.ok("Message sent to RabbitMQ and WebSocket");
-//    }
-
-
-
-        @PostMapping("/send")
-        public ResponseEntity<String> sendMessage(@RequestBody String message) {
-            rabbitTemplate.convertAndSend("my-exchange", "my-routing-key", message);
-            return ResponseEntity.ok("Message sent to RabbitMQ");
-        }
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
+        rabbitTemplate.convertAndSend("my-exchange", "my-routing-key", message);
+        return ResponseEntity.ok("Message sent to RabbitMQ");
+    }
 
     @GetMapping("/history")
     public ResponseEntity<?> getNotificationHistory() {
-
-
-        List<Object> history =    template.getForObject("http://192.168.29.67:1222/redis/history",List.class);
-
-//        ListOperations<String, Object> listOps = redisTemplate.opsForList();
-//        List<Object> history = listOps.range("notificationHistory", 0, -1);
-//
-
+        List<Object> history = template.getForObject(redisServiceUrl + "/history", List.class);
         return ResponseEntity.ok(history);
     }
-    }
-
-
-
-
-
-
-
-
-
-
-
+}
