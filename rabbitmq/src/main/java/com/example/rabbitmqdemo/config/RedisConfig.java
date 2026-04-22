edcc9e8de80bcadb21c2113ba7b064de66bd86d7
kartikeya <64151789+kartikeyasoft@users.noreply.github.com>
2026-04-22 16:52:24 +0530

    Update RedisConfig.java

--- rabbitmq/src/main/java/com/example/rabbitmqdemo/config/RedisConfig.java
+++ rabbitmq/src/main/java/com/example/rabbitmqdemo/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.rabbitmqdemo.config;
+package com.ksapp.rabbitmqdemo.config;  // Changed package
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
