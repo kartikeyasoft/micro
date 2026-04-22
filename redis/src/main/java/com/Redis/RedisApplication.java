c29efe1b623065c5906bc4b786612e50c959bc75
kartikeya <64151789+kartikeyasoft@users.noreply.github.com>
2026-04-22 14:25:26 +0530

    Update RedisApplication.java

--- redis/src/main/java/com/Redis/RedisApplication.java
+++ redis/src/main/java/com/Redis/RedisApplication.java
@@ -1,9 +1,11 @@
-package com.Redis;
+package com.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
