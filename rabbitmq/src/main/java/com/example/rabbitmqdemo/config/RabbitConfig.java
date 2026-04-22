7df3b5edd1c829bebdc20d5c06f114586c099f2f
kartikeya <64151789+kartikeyasoft@users.noreply.github.com>
2026-04-22 15:35:18 +0530

    Update RabbitConfig.java

--- rabbitmq/src/main/java/com/example/rabbitmqdemo/config/RabbitConfig.java
+++ rabbitmq/src/main/java/com/example/rabbitmqdemo/config/RabbitConfig.java
@@ -1,18 +1,44 @@
 package com.example.rabbitmqdemo.config;
 
 import org.springframework.amqp.core.*;
-
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RabbitConfig {
+
+    @Value("${spring.rabbitmq.host:localhost}")
+    private String rabbitHost;
+
+    @Value("${spring.rabbitmq.port:5672}")
+    private int rabbitPort;
+
+    @Value("${spring.rabbitmq.username:guest}")
+    private String rabbitUsername;
+
+    @Value("${spring.rabbitmq.password:guest}")
+    private String rabbitPassword;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(rabbitHost);
+        connectionFactory.setPort(rabbitPort);
+        connectionFactory.setUsername(rabbitUsername);
+        connectionFactory.setPassword(rabbitPassword);
+        return connectionFactory;
+    }
+
     @Bean
     public Queue queue() {
-        return new Queue("my-queue", false);
+        return new Queue("my-queue", true);
     }
 
     @Bean
@@ -24,9 +50,17 @@ public class RabbitConfig {
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("my-routing-key");
     }
+
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
@@ -34,4 +68,3 @@ public class RabbitConfig {
         return new RestTemplate();
     }
 }
-
