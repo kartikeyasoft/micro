3189fd2777bdc38344ab9438b84282820a2caffd
Your Name <you@example.com>
2026-04-23 12:26:35 +0530

    dfabd

--- rabbitmq/terraform/variables.tf
+++ rabbitmq/terraform/variables.tf
@@ -1,38 +1,59 @@
 variable "ami_id" {
-  type = string
+  description = "RabbitMQ Backend AMI ID"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "eureka_url" {
-  type    = string
-  default = "http://localhost:8761/eureka/"
+  description = "Eureka server URL"
+  type        = string
+  default     = "http://localhost:8761/eureka/"
 }
 
 variable "redis_service_url" {
-  type    = string
-  default = "http://localhost:1222/redis"
+  description = "Redis Service URL"
+  type        = string
+  default     = "http://localhost:1222/redis"
 }
 
 variable "environment" {
-  type    = string
-  default = "production"
+  description = "Environment name"
+  type        = string
+  default     = "production"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "subnet_id" {
-  type    = string
-  default = "subnet-0aa31e769c8f4d73e"
+  description = "Subnet ID for the instance"
+  type        = string
+  default     = "subnet-0aa31e769c8f4d73e"
 }
 
 variable "vpc_id" {
-  type    = string
-  default = "vpc-0cb7deb47a6bfa727"
+  description = "VPC ID for security group"
+  type        = string
+  default     = "vpc-0cb7deb47a6bfa727"
 }
 
 variable "key_name" {
-  type    = string
-  default = "ksansible"
+  description = "SSH key pair name"
+  type        = string
+  default     = "ksansible"
+}
+
+variable "assign_eip" {
+  description = "Assign Elastic IP to instance"
+  type        = bool
+  default     = false
 }
