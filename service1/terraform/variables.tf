388ebeac63f6cba074e94d38170e2024b733daf4
Your Name <you@example.com>
2026-04-23 11:16:32 +0530

    dvv

--- service1/terraform/variables.tf
+++ service1/terraform/variables.tf
@@ -1,48 +1,72 @@
 variable "ami_id" {
-  type = string
+  description = "Service1 AMI ID"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "eureka_url" {
-  type    = string
-  default = "http://localhost:8761/eureka/"
+  description = "Eureka server URL"
+  type        = string
+  default     = "http://localhost:8761/eureka/"
 }
 
 variable "db_url" {
-  type    = string
-  default = "jdbc:mysql://172.21.12.151:3306/exam?useSSL=false&serverTimezone=UTC"
+  description = "Database URL"
+  type        = string
+  default     = "jdbc:mysql://172.21.12.151:3306/exam?useSSL=false&serverTimezone=UTC"
 }
 
 variable "db_username" {
-  type    = string
-  default = "Admin"
+  description = "Database username"
+  type        = string
+  default     = "Admin"
 }
 
 variable "db_password" {
-  type      = string
-  sensitive = true
+  description = "Database password"
+  type        = string
+  sensitive   = true
+  default     = "Admin@123"
 }
 
 variable "environment" {
-  type    = string
-  default = "production"
+  description = "Environment name"
+  type        = string
+  default     = "production"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "subnet_id" {
-  type    = string
-  default = "subnet-0aa31e769c8f4d73e"
+  description = "Subnet ID for the instance"
+  type        = string
+  default     = "subnet-0aa31e769c8f4d73e"
 }
 
 variable "vpc_id" {
-  type    = string
-  default = "vpc-0cb7deb47a6bfa727"
+  description = "VPC ID for security group"
+  type        = string
+  default     = "vpc-0cb7deb47a6bfa727"
 }
 
 variable "key_name" {
-  type    = string
-  default = "ksansible"
+  description = "SSH key pair name"
+  type        = string
+  default     = "ksansible"
+}
+
+variable "assign_eip" {
+  description = "Assign Elastic IP to instance"
+  type        = bool
+  default     = false
 }
