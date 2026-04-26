0c68825186f2bb4fc4d29ea8971df954adb93773
Your Name <you@example.com>
2026-04-26 13:08:48 +0530

    zvbdfzv

--- service1/terraform/main.tf
+++ service1/terraform/main.tf
@@ -90,19 +90,3 @@ resource "aws_instance" "service1" {
     create_before_destroy = true
   }
 }
-
-# Outputs
-output "service1_public_ip" {
-  description = "Public IP of Service1 instance"
-  value       = aws_instance.service1.public_ip
-}
-
-output "service1_private_ip" {
-  description = "Private IP of Service1 instance"
-  value       = aws_instance.service1.private_ip
-}
-
-output "service1_security_group_id" {
-  description = "Security group ID"
-  value       = aws_security_group.service1.id
-}
